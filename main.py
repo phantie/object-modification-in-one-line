572ebca0caf5b15854916f085277fef6d08845b7
phantie <43893037+phantie@users.noreply.github.com>
2020-10-08 15:33:46 +0300

    just file, just copy the contents

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-class mockmeth:
-    def __init__(self, name, taken):
-        self.name = name
-        self.bounded = getattr(taken.obj, name)
-        self.taken = taken
-
-    def __call__(self, *args, **kwargs):
-        self.bounded(*args, **kwargs)
-        return self.taken
-
 class take:
+    class mockmeth:
+        def __init__(self, name, taken):
+            self.name = name
+            self.bounded = getattr(taken.obj, name)
+            self.taken = taken
+
+        def __call__(self, *args, **kwargs):
+            self.bounded(*args, **kwargs)
+            return self.taken
+
     def __init__(self, obj):
         self.obj = obj
 
     def __getattr__(self, name):
-        return mockmeth(name, self)
+        return self.mockmeth(name, self)
 
     def unwrap(self):
         return self.obj
